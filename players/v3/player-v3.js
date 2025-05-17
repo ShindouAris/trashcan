d539246c1bc3ab154ba9e0cd5886c54d18bf3cbb
ArisDEV <tranthanhnam.ccc@gmail.com>
2025-05-17 13:23:48 +0700

    update server address lmao

--- players/v3/player-v3.js
+++ players/v3/player-v3.js
@@ -20,7 +20,7 @@ const visualizerWrapper = document.getElementById('visualizer-container');
 const canvas = document.getElementById('accuracy-visualizer');
 const ctx = canvas.getContext('2d');
 
-const BACKEND_UPLOAD_URL = 'http://192.168.1.10:8000/replayv1';
+const BACKEND_UPLOAD_URL = 'http://40.76.122.0:8000/replayv1';
 
 
 let fullBackendData = null; // Store the entire response
--- players/v5/player-v5.js
+++ players/v5/player-v5.js
@@ -35,7 +35,7 @@ const ctx = canvas.getContext('2d');
 const fileNameDisplayElement = document.getElementById('fileNameDisplay');
 const fileSizeDisplayElement = document.getElementById('fileSizeDisplay');
 
-const BACKEND_UPLOAD_URL = 'http://192.168.1.8:8000/replayv2';
+const BACKEND_UPLOAD_URL = 'http://40.76.122.0:8000/replayv2';
 
 let allReplaysData = null;
 let currentReplayKey = null;
